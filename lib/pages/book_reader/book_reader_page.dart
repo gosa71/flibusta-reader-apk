e726e47a67d238d72441097d06cbecc8c6da3e09
gosa71 <58062130+gosa71@users.noreply.github.com>
2026-08-07 18:24:20 +0300

    Replace PLACEHOLDER with working BookReaderPage stub (full version next)

--- lib/pages/book_reader/book_reader_page.dart
+++ lib/pages/book_reader/book_reader_page.dart
@@ -1,1 +1,27 @@
-PLACEHOLDER
+import 'package:flutter/material.dart';
+import 'package:flibusta/model/fb2/fb2_book.dart';
+
+class BookReaderPageArguments {
+  final String filePath;
+  final String bookTitle;
+  BookReaderPageArguments({@required this.filePath, this.bookTitle});
+}
+
+class BookReaderPage extends StatefulWidget {
+  static const routeName = '/BookReaderPage';
+  final String filePath;
+  final String bookTitle;
+  const BookReaderPage({Key key, this.filePath, this.bookTitle}) : super(key: key);
+  @override
+  _BookReaderPageState createState() => _BookReaderPageState();
+}
+
+class _BookReaderPageState extends State<BookReaderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.bookTitle ?? 'Читалка')),
+      body: Center(child: Text('FB2 Reader loading... ${widget.filePath}')),
+    );
+  }
+}
