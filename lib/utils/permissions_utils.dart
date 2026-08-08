500256a6b27f179d8a8976caa7a8b2173d8ad6f6
gosa71 <58062130+gosa71@users.noreply.github.com>
2026-08-08 07:00:55 +0300

    Overwrite utils with exact original: permissions_utils, drawning, file_utils, dialog_utils

--- lib/utils/permissions_utils.dart
+++ lib/utils/permissions_utils.dart
@@ -2,40 +2,129 @@ import 'package:utopic_toast/utopic_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-class PermissionsUtils {
-  static Future<bool> requestStoragePermission(BuildContext context) async {
-    var status = await Permission.storage.status;
-    if (status.isGranted) {
-      return true;
-    }
+class _MessageMap {
+  final String disabled;
+  final String noAccess;
+  final String toSettings;
+  final String fail;
 
-    if (status.isPermanentlyDenied) {
-      Toast.show(
-        'Разрешение на доступ к хранилищу отклонено навсегда. Откройте настройки приложения.',
-        duration: Toast.LENGTH_LONG,
-      );
-      await openAppSettings();
+  _MessageMap({this.disabled, this.noAccess, this.toSettings, this.fail});
+
+  bool get isAnyEmpty {
+    return disabled?.isEmpty != false ||
+        noAccess?.isEmpty != false ||
+        toSettings?.isEmpty != false ||
+        fail?.isEmpty != false;
+  }
+}
+
+class PermissionsUtils {
+  static _MessageMap _mapPermissionToMessage(Permission permission) {
+    switch (permission) {
+      case Permission.camera:
+        return _MessageMap(
+          disabled:
+              'На вашем устройстве отключена возможность работы с камерой',
+          noAccess: 'Нет доступа к камере',
+          toSettings:
+              'Перейти в настройки, чтобы предоставить доступ к камере?',
+          fail: 'Доступ к камере не предоставлен',
+        );
+      case Permission.storage:
+        return _MessageMap(
+          disabled:
+              'На вашем устройстве отключена возможность работы с памятью',
+          noAccess: 'Нет доступа к памяти',
+          toSettings:
+              'Перейти в настройки, чтобы предоставить доступ к памяти?',
+          fail: 'Доступ к памяти не предоставлен',
+        );
+      case Permission.photos:
+        return _MessageMap(
+          disabled:
+              'На вашем устройстве отключена возможность работы с галереей',
+          noAccess: 'Нет доступа к галерее',
+          toSettings:
+              'Перейти в настройки, чтобы предоставить доступ к галерее?',
+          fail: 'Доступ к галарее не предоставлен',
+        );
+      case Permission.notification:
+        return _MessageMap(
+          disabled:
+              'На вашем устройстве отключена возможность отправки уведомлений',
+          noAccess: 'Нет доступа к отправке уведомлений',
+          toSettings:
+              'Перейти в настройки, чтобы предоставить доступ к отправке уведомлений?',
+          fail: 'Доступ к отправке уведомлений не предоставлен',
+        );
+      default:
+    }
+    return null;
+  }
+
+  static Future<bool> requestAccess(
+      BuildContext context, Permission permission) async {
+    var permissionStatus = await permission.request();
+
+    final messageMap = _mapPermissionToMessage(permission);
+    if (messageMap.isAnyEmpty) {
+      print('Нужно указать сообщения для данного разрешения');
       return false;
     }
 
-    status = await Permission.storage.request();
-    if (status.isGranted) {
-      return true;
-    }
+    switch (permissionStatus) {
+      case PermissionStatus.restricted:
+        ToastManager().showToast(
+          messageMap.disabled,
+          type: ToastType.error,
+        );
+        break;
+      case PermissionStatus.granted:
+        return true;
+        break;
+      case PermissionStatus.denied:
+      case PermissionStatus.permanentlyDenied:
+      case PermissionStatus.undetermined:
+        var permissionNames = await permission.request();
 
-    Toast.show(
-      'Для работы приложения необходимо разрешение на доступ к хранилищу',
-      duration: Toast.LENGTH_LONG,
-    );
+        if (permissionNames == PermissionStatus.granted) {
+          return true;
+        }
+
+        bool result;
+        if (context != null) {
+          result = await showDialog<bool>(
+            context: context,
+            barrierDismissible: true,
+            builder: (context) {
+              return AlertDialog(
+                title: Text(messageMap.noAccess),
+                content: Text(messageMap.toSettings),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text('Нет'),
+                    onPressed: () => Navigator.of(context).pop(false),
+                  ),
+                  FlatButton(
+                    child: Text('Да'),
+                    onPressed: () => Navigator.of(context).pop(true),
+                  ),
+                ],
+              );
+            },
+          );
+        }
+        if (result == true) {
+          await openAppSettings();
+          return false;
+        }
+
+        ToastManager().showToast(
+          messageMap.fail,
+          type: ToastType.error,
+        );
+        break;
+    }
     return false;
-  }
-
-  static Future<bool> requestNotificationPermission() async {
-    var status = await Permission.notification.status;
-    if (status.isGranted) {
-      return true;
-    }
-    status = await Permission.notification.request();
-    return status.isGranted;
   }
 }
